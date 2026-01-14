bc68dbbc79b781f1a7cbf95fd37484e020d10402
Pooja-250 <py2348609@gmail.com>
2026-01-14 16:50:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+/*const a = document.getElementById("company")
+a.innerText = "Capgemini"
+a.textContent = "Capgemini Pune branch"
+
+const aa=document.getElementsByClassName("revenue")[0]
+aa.innerHTML= "<h1>Budget</h1>"
+
+const  ab = document.getElementsByTagName("p")[1]
+ab.textContent = "we are testers"*/
+
+/*const ab = document.getElementsByTagName("p")
+for(let i=0; i<ab.length; i++ ){
+    ab[i].textContent = "Change paragraph"
+}*/
+
+
+//const bc = document.querySelector("#company")
+//bc.textContent = "wipro"
+
+//const ad = document.querySelector(".nav-left")
+//ab.textContent = "infosys"
+
+
+const abc = document.querySelector("header").attridutes.id
+console.log(abc)
